73119424363f0b7f47c156118effb9944dc2d0d6
Andy Kroll <cadkroll@gmail.com>
2017-10-10 11:06:59 -0500

    added roster region class variables

--- PSCreateCFNStack.ps1
+++ PSCreateCFNStack.ps1
@@ -1,1 +1,5 @@
+$roster = Get-Content "E:\GoogleDrive\Classes\ITSE1359-PowerShell\Attendance\roster-lower.txt"
+$Region = "ap-southeast-2"
+$Class = "ITSE-1359-1001"
+
 New-CFNStack -Stackname "TestingPSCFNStack" -TemplateURL "https://s3-ap-southeast-2.amazonaws.com/cf-templates-1pkm851dfqt55-ap-southeast-2/2017283GOL-StudentEnvPrivateDC.yaml" -Parameter @{ ParameterKey="STUDENTNAME"; ParameterValue="testuser" } -Region ap-southeast-2
