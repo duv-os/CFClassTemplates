bf70ce85f45c89e5b23f5bb1ee4a99863e9234cd
Andy Kroll <cadkroll@gmail.com>
2017-10-10 11:40:30 -0500

    moved param block to top

--- PSCreateCFNStack.ps1
+++ PSCreateCFNStack.ps1
@@ -1,13 +1,13 @@
+param (
+    # Parameter help description
+    $Environment
+)
 $roster = Get-Content "E:\GoogleDrive\Classes\ITSE1359-PowerShell\Attendance\roster-lower.txt"
 $Region = "ap-southeast-2"
 $Class = "ITSE-1359-1001"
 $PrivateDCTemplateURL = "https://s3-ap-southeast-2.amazonaws.com/cf-templates-1pkm851dfqt55-ap-southeast-2/2017283GOL-StudentEnvPrivateDC.yaml"
 $PublicTemplateURL = "https://s3-ap-southeast-2.amazonaws.com/cf-templates-1pkm851dfqt55-ap-southeast-2/2017283pun-StudentEnvPublic.yaml"
 
-param(
-    # Parameter help description
-    $Environment
-)
 
 foreach ($student in $roster)
 {
