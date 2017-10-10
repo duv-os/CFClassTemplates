6a02c8edb081cc2b7fd8f77d329fe1065249913d
Andy Kroll <cadkroll@gmail.com>
2017-10-10 11:11:54 -0500

    Updated region and student names with variables

--- PSCreateCFNStack.ps1
+++ PSCreateCFNStack.ps1
@@ -2,4 +2,10 @@ $roster = Get-Content "E:\GoogleDrive\Classes\ITSE1359-PowerShell\Attendance\ros
 $Region = "ap-southeast-2"
 $Class = "ITSE-1359-1001"
 
-New-CFNStack -Stackname "TestingPSCFNStack" -TemplateURL "https://s3-ap-southeast-2.amazonaws.com/cf-templates-1pkm851dfqt55-ap-southeast-2/2017283GOL-StudentEnvPrivateDC.yaml" -Parameter @{ ParameterKey="STUDENTNAME"; ParameterValue="testuser" } -Region ap-southeast-2
+foreach ($student in $roster)
+{
+    
+    New-CFNStack -Stackname "TestingPSCFNStack-$student" -TemplateURL "https://s3-ap-southeast-2.amazonaws.com/cf-templates-1pkm851dfqt55-ap-southeast-2/2017283GOL-StudentEnvPrivateDC.yaml" -Parameter @{ ParameterKey="STUDENTNAME"; ParameterValue="$student" } -Region $region
+
+}
+
