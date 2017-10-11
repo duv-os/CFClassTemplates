5e2fb6d51f495bea611d5e7bfffed1dc7bf185b9
Andy Kroll <cadkroll@gmail.com>
2017-10-11 08:38:26 -0500

    created if elseif structure and added logic to create sharedinf and autosubnet stacks

--- PSCreateCFNStack.ps1
+++ PSCreateCFNStack.ps1
@@ -11,22 +11,29 @@ $PrivateDCTemplateURL = "https://s3-ap-southeast-2.amazonaws.com/cf-templates-1p
 $BastionTemplateURL = "https://s3-ap-southeast-2.amazonaws.com/cf-templates-1pkm851dfqt55-ap-southeast-2/StudentEnvPublic.yaml"
 
 
-foreach ($student in $roster)
-{
-    if ($Environment -eq "Bastion") {
-        
+if ($Environment -eq "SharedInf") {
+    New-CFNStack -StackName "$Class-SharedInf" -TemplateURL $SharedInfTemplateURL 
+}
+
+elseif ($Environment -eq "AutoSubnet") {
+    New-CFNStack -StackName "$Class-AutoSubnet" -TemplateURL $AutoSubnetTemplateURL
+}
+elseif ($Environment -eq "Bastion") {
+    foreach ($student in $roster) {
         write-host "Creating Public CFN stack for $student"
-        New-CFNStack -Stackname "$Class-$student-Bastion" -TemplateURL $BastionTemplateURL -Parameter @{ ParameterKey="STUDENTNAME"; ParameterValue="$student" } -Region $region
+        New-CFNStack -Stackname "$Class-$student-Bastion" -TemplateURL $BastionTemplateURL -Parameter @{ ParameterKey = "STUDENTNAME"; ParameterValue = "$student" } -Region $region
         Write-Host "Finished creating stack for $student"
         pause
     }
-
-    elseif ($Environment -eq "Private") {
-        
+}
+elseif ($Environment -eq "Private") {
+    foreach ($student in $roster) {
         write-host "Creating Private DC CFN stack for $student"
-        New-CFNStack -Stackname "$Class-$student-PrivateServers" -TemplateURL $PrivateDCTemplateURL -Parameter @{ ParameterKey="STUDENTNAME"; ParameterValue="$student" } -Region $region
+        New-CFNStack -Stackname "$Class-$student-PrivateServers" -TemplateURL $PrivateDCTemplateURL -Parameter @{ ParameterKey = "STUDENTNAME"; ParameterValue = "$student" } -Region $region
         Write-Host "Finished creating stack for $student"
         pause
     }
-
-    }    
+}
+else {
+    Write-Host "No environment to make!" -ForegroundColor Red
+}
