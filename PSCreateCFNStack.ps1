d065f9ee8dbe1603c54602280e78dc7d15d33490
Andy Kroll <cadkroll@gmail.com>
2017-10-11 08:40:00 -0500

    updated roster link

--- PSCreateCFNStack.ps1
+++ PSCreateCFNStack.ps1
@@ -2,7 +2,7 @@ param (
     # Parameter help description
     $Environment
 )
-$roster = Get-Content "C:\temp\roster-lower.txt"
+$roster = Get-Content "E:\GoogleDrive\Classes\ITSE1359-PowerShell\Attendance\roster-lower.txt"
 $Region = "ap-southeast-2"
 $Class = "ITSE-1359-1001"
 $SharedInfTemplateURL = "https://s3-ap-southeast-2.amazonaws.com/cf-templates-1pkm851dfqt55-ap-southeast-2/CLASS-sharedinfrastructure.yaml"
