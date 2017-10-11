431b2cdebb7243fdb2414b2983d124e0f797719f
Andy Kroll <cadkroll@gmail.com>
2017-10-11 09:41:22 -0500

    Added verbose output

--- DeleteCFNStack.ps1
+++ DeleteCFNStack.ps1
@@ -3,5 +3,5 @@ param(
     $Environment
 )
 
-
+Write-Verbose "Deleting CloudFormation Stack"
 get-cfnstack -region $region | where {$_.stackname -like "*$Environment*"} | remove-cfnstack -region $region -Force
