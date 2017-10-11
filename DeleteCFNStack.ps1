8ecd09f83cf7be2145263cbb7c76e90648dbc6bd
Andy Kroll <cadkroll@gmail.com>
2017-10-11 09:45:28 -0500

    added some If else logic to clean up and remove buckets

--- DeleteCFNStack.ps1
+++ DeleteCFNStack.ps1
@@ -3,5 +3,12 @@ param(
     $Environment
 )
 
-Write-Verbose "Deleting CloudFormation Stack"
-get-cfnstack -region $region | where {$_.stackname -like "*$Environment*"} | remove-cfnstack -region $region -Force
+if ($Environment -eq "SharedInf") {
+    $bucket = get-cfnstack -Region $region | where {$_.StackName -like "*$Environment*"} | select -expand  outputs | Where {$_.OutputKey -eq "lambdabucket"}
+    Remove-S3Bucket -BucketName $bucket.OutputValue -deletebucketcontent
+    get-cfnstack -region $region | where {$_.stackname -like "*$Environment*"} | remove-cfnstack -region $region -Force
+}
+else {
+    Write-Verbose "Deleting CloudFormation Stack"
+    get-cfnstack -region $region | where {$_.stackname -like "*$Environment*"} | remove-cfnstack -region $region -Force
+    }
