be10c4a8de0fc7b7355812dd485f16a5ecb63615
Andy Kroll <cadkroll@gmail.com>
2017-10-10 21:31:06 -0500

    Updated template url values

--- PSCreateCFNStack.ps1
+++ PSCreateCFNStack.ps1
@@ -2,11 +2,11 @@ param (
     # Parameter help description
     $Environment
 )
-$roster = Get-Content "E:\GoogleDrive\Classes\ITSE1359-PowerShell\Attendance\roster-lower.txt"
+$roster = Get-Content "C:\temp\roster-lower.txt"
 $Region = "ap-southeast-2"
 $Class = "ITSE-1359-1001"
-$PrivateDCTemplateURL = "https://s3-ap-southeast-2.amazonaws.com/cf-templates-1pkm851dfqt55-ap-southeast-2/StudentEnvPrivateDC.yaml"
-$PublicTemplateURL = "https://s3-ap-southeast-2.amazonaws.com/cf-templates-1pkm851dfqt55-ap-southeast-2/StudentEnvPublic.yaml"
+$PrivateDCTemplateURL = "https://s3-ap-southeast-2.amazonaws.com/cf-templates-1pkm851dfqt55-ap-southeast-2/2017284IB9-StudentEnvPrivateDC.yaml"
+$PublicTemplateURL = "https://s3-ap-southeast-2.amazonaws.com/cf-templates-1pkm851dfqt55-ap-southeast-2/2017283GiX-StudentEnvPublic.yaml"
 
 
 foreach ($student in $roster)
