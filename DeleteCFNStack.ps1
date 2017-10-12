c4d10ea698c1c7ba0346ed42121061d275f6c0fd
Andy Kroll <cadkroll@gmail.com>
2017-10-12 14:20:21 -0500

    Added validateset decorator on region parameter to include all region codes

--- DeleteCFNStack.ps1
+++ DeleteCFNStack.ps1
@@ -1,5 +1,6 @@
 param(
     [Parameter(Mandatory=$True)]
+    [ValidateSet ("us-east-2", "us-east-1", "us-west-1", "us-west-2", "ca-central-1", "ap-south-1", "ap-northeast-2", "ap-southeast-1", "ap-southeast-2", "ap-northeast-1", "eu-central-1", "eu-west-1", "eu-west-2", "sa-east-1")]
     $region = "ap-southeast-2",
     [Parameter(Mandatory=$True)]
     [ValidateSet ("SharedInf","AutoSubnet","Bastion","Private")]
