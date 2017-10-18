4ceba5cf8c2cde3aaa39918ce87532107adbea16
Andy Kroll <cadkroll@gmail.com>
2017-10-18 13:40:12 -0500

    Added Commands to create stacks

---
+++ WomenInTechnologyCommands.ps1
@@ -0,0 +1,2 @@
+﻿.\CreateCFNStack.ps1 -Region ap-southeast-1 -Class WomenInTechnology -Environment SharedInf
+.\CreateCFNStack.ps1 -Region ap-southeast-1 -Class WomenInTechnology -Environment AutoSubnet
