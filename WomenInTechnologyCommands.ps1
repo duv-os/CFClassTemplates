34d9e453301ad8d918a6ec1df7cfba6b2f1dd5d7
Andy Kroll <cadkroll@gmail.com>
2017-10-18 14:11:51 -0500

    added more commands

--- WomenInTechnologyCommands.ps1
+++ WomenInTechnologyCommands.ps1
@@ -1,2 +1,11 @@
-﻿.\CreateCFNStack.ps1 -Region ap-southeast-1 -Class WomenInTechnology -Environment SharedInf
-.\CreateCFNStack.ps1 -Region ap-southeast-1 -Class WomenInTechnology -Environment AutoSubnet
+﻿.\CreateCFNStack.ps1 -Region ap-southeast-2 -Class WomenInTechnology -Environment SharedInf
+
+.\CreateCFNStack.ps1 -Region ap-southeast-2 -Class WomenInTechnology -Environment AutoSubnet
+
+.\CreateCFNStack.ps1 -Region ap-southeast-2 -Class WomenInTechnology -ClassRoster E:\Git\CFClassTemplates\WomenInTechnologyWorkstations.txt -ServerOS AMALINUX -Environment Bastion
+
+.\DeleteCFNStack.ps1 -region ap-southeast-2 -Environment Bastion
+
+.\DeleteCFNStack.ps1 -region ap-southeast-2 -Environment AutoSubnet
+
+.\DeleteCFNStack.ps1 -region ap-southeast-2 -Environment SharedInf
